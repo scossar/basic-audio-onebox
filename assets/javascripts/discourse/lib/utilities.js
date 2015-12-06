d8b5ab07898aeca69f927693b7bd0fbc86381ed5
scossar <scossar3@gmail.com>
2015-12-06 15:43:49 -0800

    don't use the onebox

--- assets/javascripts/discourse/lib/utilities.js
+++ assets/javascripts/discourse/lib/utilities.js
@@ -2,30 +2,16 @@ Discourse.Utilities.isAudioFile = function(path) {
   return (/\.(wav|mp3|ogg)$/i).test(path);
 };
 
-Discourse.Utilities.uploadBaseUrl = function() {
-  if (Discourse.S3BaseUrl) {
-    return 'https:';
-  } else {
-    var protocol = window.location.protocol + '//',
-      hostname = window.location.hostname,
-      port = ':' + window.location.port;
-    if (port) {
-      return protocol + hostname + port;
-    } else {
-      return protocol + hostname;
-    }
-  }
-};
-
+// Do something with this
 Discourse.Utilities.filenameWithoutExtension = function(filename) {
   return filename.split('.')[0];
-}
+};
 
 Discourse.Utilities.getUploadMarkdown = function(upload) {
   if (Discourse.Utilities.isAnImage(upload.original_filename)) {
     return '<img src="' + upload.url + '" width="' + upload.width + '" height="' + upload.height + '">';
   } else if (Discourse.Utilities.isAudioFile(upload.original_filename)) {
-    return Discourse.Utilities.uploadBaseUrl() + upload.url + '?filename=' + Discourse.Utilities.filenameWithoutExtension(upload.original_filename);
+    return '<audio controls><source="' + upload.url + '"><a href="' + upload.url + '">upload.url</a></audio>'
   } else {
     return '<a class="attachment" href="' + upload.url + '">' + upload.original_filename + '</a> (' + I18n.toHumanSize(upload.filesize) + ')';
   }
--- plugin.rb
+++ plugin.rb
@@ -7,3 +7,20 @@
 enabled_site_setting :basic_audioplayer_enabled
 
 register_asset 'javascripts/discourse/lib/utilities.js'
+
+
+after_initialize do
+  FileHelper.class_eval do
+    def self.is_audio_file?(filename)
+      filename =~ audio_files_regexp
+    end
+
+    def self.audio_files
+      @@audio_files ||= Set.new ["mp3", "ogg", "wav"]
+    end
+
+    def audio_files_regexp
+      @@audio_files_regexp ||= /\.(#{audio_files.to_a.join("|")})$/i
+    end
+  end
+end
