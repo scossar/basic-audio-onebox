0c680045d4f769e1f1e85addc0748506fcf8fa6c
scossar <scossar3@gmail.com>
2015-12-06 15:51:00 -0800

    check for audio files

--- plugin.rb
+++ plugin.rb
@@ -23,4 +23,17 @@ after_initialize do
       @@audio_files_regexp ||= /\.(#{audio_files.to_a.join("|")})$/i
     end
   end
+
+  Email::Receiver.class_eval do
+    def attachment_markdown(upload)
+      if FileHelper.is_image?(upload.original_filename)
+        "<img src='#{upload.url}' width='#{upload.width}' height='#{upload.height}'>"
+      elsif FileHelper.is_audio_file?(filename)
+        "<audio controls><source src='#{upload.url}'><a href='#{upload.url}'>#{upload.url}</a></audio>"
+      else
+        "<a class='attachment' href='#{upload.url}'>#{upload.original_filename}</a> (#{number_to_human_size(upload.filesize)})"
+      end
+    end
+
+  end
 end
