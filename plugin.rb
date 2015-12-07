9c05f04e488b66e3c620f1c4b7dd2b901b6a42ae
scossar <scossar3@gmail.com>
2015-12-06 23:23:39 -0800

    give filename as argument

--- plugin.rb
+++ plugin.rb
@@ -8,7 +8,6 @@ enabled_site_setting :basic_audioplayer_enabled
 
 register_asset 'javascripts/discourse/lib/utilities.js'
 
-
 after_initialize do
   FileHelper.class_eval do
     def self.is_audio_file?(filename)
@@ -31,18 +30,16 @@ after_initialize do
       else
         Discourse.base_url
       end
-
     end
 
     def attachment_markdown(upload)
       if FileHelper.is_image?(upload.original_filename)
         "<img src='#{upload.url}' width='#{upload.width}' height='#{upload.height}'>"
-      elsif FileHelper.is_audio_file?(filename)
+      elsif FileHelper.is_audio_file?(upload.original_filename)
         upload_base_url + upload.url
       else
         "<a class='attachment' href='#{upload.url}'>#{upload.original_filename}</a> (#{number_to_human_size(upload.filesize)})"
       end
     end
-
   end
 end
