6cee63b3fdd2ef1b3a78fecfa9897b3c08eea8c2
scossar <scossar3@gmail.com>
2015-12-07 15:00:55 -0800

    add newlines to attachment

--- plugin.rb
+++ plugin.rb
@@ -36,45 +36,46 @@ after_initialize do
       if FileHelper.is_image?(upload.original_filename)
         "<img src='#{upload.url}' width='#{upload.width}' height='#{upload.height}'>"
       elsif FileHelper.is_audio_file?(upload.original_filename)
-        upload_base_url + upload.url
+        # upload_base_url + upload.url
+        "\n#{upload_base_url}#{upload.url}\n"
       else
         "<a class='attachment' href='#{upload.url}'>#{upload.original_filename}</a> (#{number_to_human_size(upload.filesize)})"
       end
     end
 
-    def create_post_with_attachments(user, post_options={})
-      options = {
-          cooking_options: { traditional_markdown_linebreaks: true },
-      }.merge(post_options)
+    # def create_post_with_attachments(user, post_options={})
+    #   options = {
+    #       cooking_options: { traditional_markdown_linebreaks: true },
+    #   }.merge(post_options)
 
-      raw = options[:raw]
+      # raw = options[:raw]
 
       # deal with attachments
-      @message.attachments.each do |attachment|
-        tmp = Tempfile.new("discourse-email-attachment")
-        begin
+      # @message.attachments.each do |attachment|
+      #   tmp = Tempfile.new("discourse-email-attachment")
+      #   begin
           # read attachment
-          File.open(tmp.path, "w+b") { |f| f.write attachment.body.decoded }
+          # File.open(tmp.path, "w+b") { |f| f.write attachment.body.decoded }
           # create the upload for the user
-          upload = Upload.create_for(user.id, tmp, attachment.filename, tmp.size)
-          if upload && upload.errors.empty?
+          # upload = Upload.create_for(user.id, tmp, attachment.filename, tmp.size)
+          # if upload && upload.errors.empty?
             # try to inline images
-            if attachment.content_type.start_with?("image/")
-              if raw =~ /\[image: Inline image \d+\]/
-                raw.sub!(/\[image: Inline image \d+\]/, attachment_markdown(upload))
-                next
-              end
-            end
-            raw << "\n\n#{attachment_markdown(upload)}\n\n"
-          end
-        ensure
-          tmp.close!
-        end
-      end
+            # if attachment.content_type.start_with?("image/")
+            #   if raw =~ /\[image: Inline image \d+\]/
+            #     raw.sub!(/\[image: Inline image \d+\]/, attachment_markdown(upload))
+            #     next
+            #   end
+            # end
+            # raw << "\n\n#{attachment_markdown(upload)}\n\n"
+          # end
+        # ensure
+        #   tmp.close!
+        # end
+      # end
 
-      options[:raw] = raw
+      # options[:raw] = raw
 
-      create_post(user, options)
-    end
+      # create_post(user, options)
+    # end
   end
 end
